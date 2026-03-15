770a260d4cec4e0496f176135bf6055f98f77c64
Mahesh S Jadhav <80886867+jadhavmahesh4791@users.noreply.github.com>
2026-03-15 17:01:04 +0530

    Add AWS provider and S3 bucket configuration

---
+++ main.tf
@@ -0,0 +1,14 @@
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1" # Specify your desired region
+}
+
+# Create an S3 bucket
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-tf-enterprise-bucket"
+
+  tags = {
+    Name        = "MyS3Bucket"
+    Environment = "Production"
+  }
+}
