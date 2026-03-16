f633d969bf15928e0b05791d649ef375589991e5
Mahesh S Jadhav <80886867+jadhavmahesh4791@users.noreply.github.com>
2026-03-16 15:27:59 +0530

    Remove commented S3 bucket resource
    Removed commented-out S3 bucket resource definition.

--- main.tf
+++ main.tf
@@ -12,13 +12,3 @@ resource "aws_s3_bucket" "my_bucket" {
     Environment = "Production"
   }
 }
-
-# Create an S3 bucket
-resource "aws_s3_bucket" "my_bucket-1" {
-  bucket = "my-tf-enterprise-bucket-1"
-
-  tags = {
-    Name        = "MyS3Bucket"
-    Environment = "Production"
-  }
-}
